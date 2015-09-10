0a5535b247fb5d944f619e11704b916572f4674d
sandip <k.sandip96@gmail.com>
2015-09-10 12:58:49 +0530

    Refactoring

--- JobScripts/dockerScript/create_container_html.sh
+++ JobScripts/dockerScript/create_container_html.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-source /var/lib/jenkins/JobScripts/jenkins_functions.sh
+source /var/lib/jenkins/JobScripts/jenkinsScript/jenkins_functions.sh
 source /var/lib/jenkins/JobScripts/util_functions.sh
 
-createCell() {
+create_cell() {
 	cellText=$1
 	htmlFile=$2
 
@@ -12,7 +12,7 @@ createCell() {
 	echo "</td>" >> $htmlFile
 }
 
-createCellFromFileContent() {
+create_cell_from_file_content() {
         filePath=$1
         htmlFile=$2
 
@@ -28,11 +28,10 @@ createCellFromFileContent() {
 
 }
 
-createCellFromJobStatus() {
+create_cell_from_job_status() {
 	jobName=$1
 	echo $jobName
         html=$2
-	get_job_status $jobName
         jobStatus=`get_job_status $jobName`
 	echo "Status of job $jobName is $jobStatus"
 	echo "<td>" >> $htmlFile
@@ -40,7 +39,7 @@ createCellFromJobStatus() {
         echo "</td>" >> $htmlFile
 }
 
-createCellFromMultiJobStatus() {
+create_cell_from_multi_job_status() {
         jobName=$1
 	echo $jobName
         html=$2
@@ -50,7 +49,7 @@ createCellFromMultiJobStatus() {
         echo "</td>" >> $htmlFile
 }
 
-createExecuteWrapperJobButton(){
+create_execute_wrapper_job_button(){
 	htmlFile=$1
 	branch=$2
 	echo "<td>" >> $htmlFile
@@ -61,48 +60,51 @@ createExecuteWrapperJobButton(){
 create_container_html() {
 filename=$1
 path=$2
-echo "<html>" > $path
-echo "<head>" >> $path
-echo "<title>" >> $path
-echo "Server mappings info" >> $path
-echo "</title></head>" >> $path
-echo "<body>" >> $path
-echo "<table width='100%' border='2'>" >> $path
-echo "<tr>" >> $path
-	echo "<th> Server name </th><th> SSH Port </th><th> VNC Port </th><th> Web Server Port </th><th> Wrapper Code Quality Status </th><th> Merge Status </th><th> Unit Tests Status </th><th> Functional Test Status </th><th> Static Code Analysis Status </th></tr>" >> $path
+
+cat <<EOF >> $path
+<html>
+<head>
+<title>
+Server mappings info
+</title></head>
+<body>
+<table width='100%' border='2'>
+<tr>
+<th> Server name </th><th> SSH Port </th><th> VNC Port </th><th> Web Server Port </th><th> Wrapper Code Quality Status </th><th> Merge Status </th><th> Unit Tests Status </th><th> Functional Test Status </th><th> Static Code Analysis Status </th></tr>
+EOF
+
 while read -r line
 do
 	name=$line
 	echo "<tr>" >> $path
-	createCell `echo "$name" | cut -d " " -f1` $path
+	create_cell `echo "$name" | cut -d " " -f1` $path
 
-	createCell `echo "$name" | cut -d " " -f2` $path
+	create_cell `echo "$name" | cut -d " " -f2` $path
 
-	createCell `echo "$name" | cut -d " " -f3` $path
+	create_cell `echo "$name" | cut -d " " -f3` $path
 
-	createCell `echo "$name" | cut -d " " -f4` $path
+	create_cell `echo "$name" | cut -d " " -f4` $path
 
 	current_server=`echo "$name" | cut -d " " -f1`
 	current_branch=`echo ${current_server/_Server}`
 
-	createCellFromMultiJobStatus "${current_branch}MultiJob" $path
+	create_cell_from_multi_job_status "${current_branch}MultiJob" $path
 
 	if [ ! -f "/var/lib/jenkins/jobs/${current_branch}MultiJob/mergestatus.txt" ]; then
                 touch "/var/lib/jenkins/jobs/${current_branch}MultiJob/mergestatus.txt" 
 		echo "NoMergesYet" >> "/var/lib/jenkins/jobs/${current_branch}MultiJob/mergestatus.txt"
         fi
 
-	createCellFromFileContent /var/lib/jenkins/jobs/${current_branch}MultiJob/mergestatus.txt $path
+	create_cell_from_file_content /var/lib/jenkins/jobs/${current_branch}MultiJob/mergestatus.txt $path
 
-	createCellFromJobStatus "${current_branch}UnitTest" $path
+	create_cell_from_job_status "${current_branch}UnitTest" $path
 	
-	createCellFromJobStatus "${current_branch}FunctionalTest" $path
+	create_cell_from_job_status "${current_branch}FunctionalTest" $path
 	
-	createCellFromJobStatus "${current_branch}StaticCodeAnalysis" $path
+	create_cell_from_job_status "${current_branch}StaticCodeAnalysis" $path
 
-	createExecuteWrapperJobButton $path ${current_branch}
-#	echo "</table>" >> $path
-#	echo "</td>" >> $path
+	create_execute_wrapper_job_button $path ${current_branch}
+
 	echo "</tr>" >> $path
 done < "$filename"
 echo "</table>" >> $path
